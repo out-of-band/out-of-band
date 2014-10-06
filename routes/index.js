6e7bdc747842219e3c95993545a6c5e59ee23aed
codyf_000 <codyfarkas@gmail.com>
2014-10-05 21:14:00 -0400

    Updated links

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Scott Collier' });
+  res.render('index', { title: 'scottcollier' });
 });
 
 module.exports = router;
