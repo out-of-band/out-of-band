ec47a0e757385b960fb073eb3f5b9c740232dab5
codyf_000 <codyfarkas@gmail.com>
2014-10-05 21:03:02 -0400

    Updated links

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-logger.info('Starting express.');
 var app = express();
 
 // view engine setup
